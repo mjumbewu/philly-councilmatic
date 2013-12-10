931f6fb6fca09d17cd690c252d7dce1a48a80fce
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-12-09 22:42:27 -0500

    Update debug toolbar settings

--- src/settings.py
+++ src/settings.py
@@ -223,10 +223,24 @@ SOUTH_TESTS_MIGRATE = False
 
 # Debugging
 DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': True,
-    'SHOW_TOOLBAR_CONFIG': (lambda: DEBUG)
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': (lambda request: DEBUG)
 }
 INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+)
 
 # Logging
 # -------
