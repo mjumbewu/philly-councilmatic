821660d9ce0def44f55f23862ff84b961c9d1a68
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-12-17 21:32:36 -0500

    Update the staticroot

--- src/settings.py
+++ src/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = rel_path('..', 'static')
+STATIC_ROOT = rel_path('staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
