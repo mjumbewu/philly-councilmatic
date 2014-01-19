e5949b3cb277039e4e9514cec493fdae8eb6a49a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-01-18 21:16:40 -0500

    Use non-geo BD in main settings file
    On Heroku, we were getting an error where the collectstatic task was never
    being run because of an error related to loading the GEOS libraries. So, for
    future reference, Heroku should never have to load GEOS during deploy (even
    though it seems like it should be installed just fine using multi-buildpack).
    
    Somehow, even though we include django.contrib.gis in INSTALLED_APPS and use
    the postgis backend in the heroku_settings module, these don't cause problems.

--- src/settings.py
+++ src/settings.py
@@ -28,7 +28,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
