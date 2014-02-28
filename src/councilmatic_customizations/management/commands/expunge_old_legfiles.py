6fb5a8495c84355d0f1f85af8b0f773bb21140bc
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-02-28 03:05:02 -0500

    Use the first group of the regex match

--- src/councilmatic_customizations/management/commands/expunge_old_legfiles.py
+++ src/councilmatic_customizations/management/commands/expunge_old_legfiles.py
@@ -42,21 +42,26 @@ class Command(BaseCommand):
                 prev_host = domain_pattern.match(prev.url)
                 curr_host = domain_pattern.match(curr.url)
 
-                if 'legistar.com' == prev_host and curr_host != 'legistar.com':
-                    should_delete = curr
-                    delete_host = curr_host
-                elif 'legistar.com' != prev_host and curr_host == 'legistar.com':
-                    should_delete = prev
-                    delete_host = prev_host
-                    prev = curr
-                else:
-                    log.info('Wait, I\'m confused. Both of these have domains %s and %s.' % (curr_host, prev_host))
-                    sys.exit(1)
+                if prev_host and curr_host:
+                    prev_host = prev_host.group(1)
+                    curr_host = curr_host.group(1)
+
+                    if 'legistar.com' == prev_host and curr_host != 'legistar.com':
+                        should_delete = curr
+                        delete_host = curr_host
+                    elif 'legistar.com' != prev_host and curr_host == 'legistar.com':
+                        should_delete = prev
+                        delete_host = prev_host
+                        prev = curr
+                    else:
+                        log.info('Wait, I\'m confused. Both of these have domains %s and %s.' % (curr_host, prev_host))
+                        sys.exit(1)
 
                 if should_delete:
                     log.info('Key %s is a duplicate. One has host legistar. One with host %s will be deleted.' % (prev_id, delete_host))
                     if not dry_run:
                         should_delete.delete()
+                    should_delete = None
 
             else:
                 prev = curr
