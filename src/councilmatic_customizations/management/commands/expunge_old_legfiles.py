e7abbdb01fd4bbf8c41f2a2c3feecfaa5bec291c
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-02-28 02:46:41 -0500

    Add expunge_old_legfiles command

---
+++ src/councilmatic_customizations/management/commands/expunge_old_legfiles.py
@@ -0,0 +1,63 @@
+###############################################################################
+# This will collect the latest legislative filings released in the city of
+# Philadelphia.
+###############################################################################
+
+#will send out daily email for users - first will read all keywords
+#create text files, then email text files to all each user subscribed.
+
+from django.core.management.base import BaseCommand, CommandError
+import logging
+import optparse
+import re
+import sys
+
+from phillyleg.models import LegFile
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Get rid of duplicate legislation from the older city-hosted legistar site."
+    option_list = BaseCommand.option_list + (
+            optparse.make_option('--dry-run',
+                action='store_true',
+                dest='dry_run',
+                default=False,
+                help='Don\' actually make any DB changes'),
+            )
+
+    def handle(self, *args, **options):
+        dry_run = options['dry_run']
+
+        domain_pattern = re.compile(r'http://.*([^\.+]\.[^\.+])/.*$')
+        legfiles = LegFile.objects.all().order_by('id')
+        prev = prev_id = prev_host = None
+
+        for curr in legfiles:
+            should_delete = None
+            delete_host = None
+
+            if curr.id == prev_id:
+                prev_host = domain_pattern.match(prev.url)
+                curr_host = domain_pattern.match(curr.url)
+
+                if 'legistar.com' == prev_host and curr_host != 'legistar.com':
+                    should_delete = curr
+                    delete_host = curr_host
+                elif 'legistar.com' != prev_host and curr_host == 'legistar.com':
+                    should_delete = prev
+                    delete_host = prev_host
+                    prev = curr
+                else:
+                    log.info('Wait, I\'m confused. Both of these have domains %s and %s.' % (curr_host, prev_host))
+                    sys.exit(1)
+
+                if should_delete:
+                    log.info('Key %s is a duplicate. One has host legistar. One with host %s will be deleted.' % (prev_id, delete_host))
+                    if not dry_run:
+                        should_delete.delete()
+
+            else:
+                prev = curr
+                prev_id = curr.id
